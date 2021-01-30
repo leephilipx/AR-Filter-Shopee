08ba944046e244dd85e07f2dc2a0f0e1ba5eb730
Philip Lee <leephiliphy@gmail.com>
2021-01-31 02:42:51 +0800

    submission

---
+++ facedetection_flutter/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,33 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<camera/CameraPlugin.h>)
+#import <camera/CameraPlugin.h>
+#else
+@import camera;
+#endif
+
+#if __has_include(<firebase_ml_vision/FirebaseMlVisionPlugin.h>)
+#import <firebase_ml_vision/FirebaseMlVisionPlugin.h>
+#else
+@import firebase_ml_vision;
+#endif
+
+#if __has_include(<image_picker/FLTImagePickerPlugin.h>)
+#import <image_picker/FLTImagePickerPlugin.h>
+#else
+@import image_picker;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [CameraPlugin registerWithRegistrar:[registry registrarForPlugin:@"CameraPlugin"]];
+  [FLTFirebaseMlVisionPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseMlVisionPlugin"]];
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+}
+
+@end
